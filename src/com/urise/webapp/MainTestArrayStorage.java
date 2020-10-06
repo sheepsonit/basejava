bad4e4004a99cad9e2f4607d43f930e6c245f6d2
Svetlana Buyukova <buyukova@mail.ru>
2020-10-06 12:48:50 +0300

    add static class members

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -10,13 +10,13 @@ public class MainTestArrayStorage {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
+        final Resume r1 = new Resume();
         r1.setUuid("uuid1");
-        Resume r2 = new Resume();
+        final Resume r2 = new Resume();
         r2.setUuid("uuid2");
-        Resume r3 = new Resume();
+        final Resume r3 = new Resume();
         r3.setUuid("uuid3");
-        Resume r4 = new Resume();
+        final Resume r4 = new Resume();
         r4.setUuid("uuid1");
 
         ARRAY_STORAGE.save(r1);
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,7 +8,8 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10_000];
+    private static final int STORAGE_LIMIT = 10_000;
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -26,7 +27,7 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        if (size == storage.length) {
+        if (size == STORAGE_LIMIT) {
             System.out.println("Storage is overflow");
         } else if (getIndex(resume.getUuid()) > -1) {
             System.out.println("Can`t save resume. Resume with uuid: " + resume.getUuid() + " already exist");
