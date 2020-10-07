580ac220d610245fe0145e51baa3d6fcb4fbe75a
Svetlana Buyukova <buyukova@mail.ru>
2020-10-07 14:00:54 +0300

    Fixed flaws

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -12,6 +12,10 @@ public class Resume implements Comparable<Resume> {
         return uuid;
     }
 
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -25,10 +29,6 @@ public class Resume implements Comparable<Resume> {
     @Override
     public int hashCode() {
         return uuid.hashCode();
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
     }
 
     @Override
