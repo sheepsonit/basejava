7346a42ae414b0859b81d54660b1121d40b1bec5
Svetlana Buyukova <buyukova@mail.ru>
2020-10-12 21:57:01 +0300

    Refactoring method getIndex for SortedArrayStorage

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -8,8 +8,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     protected int getIndex(String uuid) {
-        Resume searchKey = new Resume(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, new Resume(uuid));
     }
 
     @Override
