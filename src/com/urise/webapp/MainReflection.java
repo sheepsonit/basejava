0f7d0b088b96a9bbca05b46aced668cf83d558c4
Svetlana Buyukova <buyukova@mail.ru>
2020-10-12 16:40:48 +0300

    Called toString method with Reflection API

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -3,15 +3,18 @@ package com.urise.webapp;
 import com.urise.webapp.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume resume = new Resume();
         Field field = resume.getClass().getDeclaredFields()[0];
         System.out.println(field.getName());
         field.setAccessible(true);
         field.get(resume);
         field.set(resume,"new UUID");
-        System.out.println(resume);
+        Method toStringMethod = resume.getClass().getMethod("toString");
+        System.out.println(toStringMethod.invoke(resume));
     }
 }
