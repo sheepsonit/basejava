2f1479e078e6f37b51d1d8203c9b7b24c22b91b9
Svetlana Buyukova <buyukova@mail.ru>
2020-11-04 14:32:43 +0300

    Added enum SectionType

---
+++ src/com/urise/webapp/TestSingleton.java
@@ -0,0 +1,28 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.SectionType;
+
+public class TestSingleton {
+    private static TestSingleton instance;
+
+    public static TestSingleton getInstance() {
+        if (instance == null)
+            instance = new TestSingleton();
+        return instance;
+    }
+
+    private TestSingleton() {}
+
+    public static void main(String[] args) {
+        System.out.println(TestSingleton.getInstance().toString());
+        Singleton instance = Singleton.valueOf("INSTANCE");
+        System.out.println(instance);
+        for (SectionType type : SectionType.values()) {
+            System.out.println(type.getTitle());
+        }
+    }
+
+    public enum Singleton {
+        INSTANCE
+    }
+}
