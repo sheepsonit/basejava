b9e0bf639e7062ee1d9c7546c2b53edbc75678c1
Svetlana Buyukova <buyukova@mail.ru>
2021-03-01 16:04:06 +0300

    Refactoring minValue and oddOrEven methods.

--- src/com/urise/webapp/MainStreams.java
+++ src/com/urise/webapp/MainStreams.java
@@ -2,7 +2,10 @@ package com.urise.webapp;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class MainStreams {
     public static void main(String[] args) {
@@ -13,19 +16,20 @@ public class MainStreams {
     }
 
     private static int minValue(int[] values) {
+        AtomicInteger i = new AtomicInteger(1);
         return Arrays.stream(values).
                 distinct().
-                sorted().
-                reduce((x, y) -> Integer.parseInt(Integer.toString(x).concat(Integer.toString(y)))).
-                getAsInt();
+                boxed().
+                sorted(Comparator.reverseOrder()).
+                reduce(0, (x, y) -> {
+                    x += y * i.get();
+                    i.updateAndGet(v -> v * 10);
+                    return x;
+                });
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        if (integers.stream().reduce(Integer::sum).get() % 2 == 0) {
-            integers.removeIf(n -> n % 2 == 0);
-        } else {
-            integers.removeIf(n -> n % 2 != 0);
-        }
-        return integers;
+        boolean isEven = integers.stream().reduce(Integer::sum).get() % 2 == 0;
+        return integers.stream().filter(isEven ? n -> n % 2 != 0 : n -> n % 2 == 0).collect(Collectors.toList());
     }
 }
