ccb1ac01067574a6a01843cf6791edcc372800d1
Svetlana Buyukova <buyukova@mail.ru>
2021-04-07 10:21:35 +0300

    Added psql driver initialization

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    protected static final File PROPS = new File("config/resumes.properties");
+    protected static final File PROPS = new File("D:\\JavaProjects\\basejavaNew\\config\\resumes.properties");
     private static final Config INSTANCE = new Config();
     private Properties props = new Properties();
     private File storageDir;
--- src/com/urise/webapp/storage/SqlStorage.java
+++ src/com/urise/webapp/storage/SqlStorage.java
@@ -13,6 +13,11 @@ public class SqlStorage implements Storage {
     private final SqlHelper sqlHelper;
 
     public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("No driver found for PostgreSQL database", e);
+        }
         sqlHelper = new SqlHelper(() -> DriverManager.getConnection(dbUrl, dbUser, dbPassword));
     }
 
